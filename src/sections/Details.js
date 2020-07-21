643f818c197078a77890ff80177617f7c4ec052c
Alex Tan <alextan785@gmail.com>
2020-07-20 19:47:17 -0700

    hours styled. need image?

--- src/sections/Details.js
+++ src/sections/Details.js
@@ -4,18 +4,21 @@ import MapContainer from "../components/MapContainer.js";
 
 import { groupHours } from "../helpers/hoursParser";
 
-// TODO: Hours container is hardcoded width, whats the big idea here?
-// TODO: Redo hours to date string to fit https://www.google.com/search?q=restaurant+hours+design&sxsrf=ALeKk02IPnUX6aihrYwiSeuvpCnFpboUwQ:1595274473309&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiv0LPCzNzqAhUTNn0KHRohAi0Q_AUoAXoECA8QAw&cshid=1595274661669336&biw=1680&bih=868#imgrc=lkIbizCJrTwRfM
+// TODO: Add an image of the building next to the hours?
 const phoneNumber = "(206) 363-5100";
 export default class Details extends React.Component {
   createHoursTable = (openHours) => {
     if (Object.keys(openHours).length === 0 && openHours.constructor === Object) return null;
 
     const hours = groupHours(openHours);
-    const rows = hours.map((hour) => {
+    const length = hours.length - 1;
+    const rows = hours.map((hour, index) => {
       const { dayRange, timeRange } = hour;
+      let addedStyle;
+      if (index === 0) addedStyle = { marginTop: 0 };
+      else if (index === length) addedStyle = { marginBottom: 0 };
       return (
-        <div key={dayRange} className="hours-row">
+        <div key={dayRange} className="hours-row" style={addedStyle}>
           <p className="day">{dayRange}</p>
           <p className="range">{timeRange}</p>
         </div>
@@ -24,8 +27,11 @@ export default class Details extends React.Component {
     return (
       <div className="hours-container">
         <div className="hours-descr">
-          <h3>Opening Hours</h3>
-          <p>{`Tel: ${phoneNumber}`}</p>
+          <div>
+            <p className="opening-hours">{`Opening`}</p>
+            <p className="opening-hours">{`Hours`}</p>
+          </div>
+          <p className="phone">{`Tel: ${phoneNumber}`}</p>
         </div>
         <div className="border" />
         <div style={{ display: "flex", flexDirection: "column" }}>{rows}</div>
@@ -41,9 +47,9 @@ export default class Details extends React.Component {
       <section id="details" className="details-container">
         <div className="details">
           <div className="hours-table">{hoursTable}</div>
-          {/* <div className="inline-block map-container">
+          <div className="inline-block map-container">
             <MapContainer />
-          </div> */}
+          </div>
         </div>
       </section>
     );
