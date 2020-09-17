a1d26d3ad16ebf29d55ef17f57027a2b3a56cc0b
Alex Tan <alextan785@gmail.com>
2020-09-17 12:41:18 -0700

    Updates

---
+++ precache-manifest.e99337105405999563ba2c6c2ad1f299.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8bc807cac2744e2d257b8b84a4c1205",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49b29722ab5ee2d3d78d",
+    "url": "/static/css/2.dd1866a9.chunk.css"
+  },
+  {
+    "revision": "4f94130df0fa022ed7ea",
+    "url": "/static/css/main.85db960c.chunk.css"
+  },
+  {
+    "revision": "49b29722ab5ee2d3d78d",
+    "url": "/static/js/2.f9302f27.chunk.js"
+  },
+  {
+    "revision": "2c2a4e9dc7fde790ac8918151a418532",
+    "url": "/static/js/2.f9302f27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f94130df0fa022ed7ea",
+    "url": "/static/js/main.83accf79.chunk.js"
+  },
+  {
+    "revision": "d40a3d73069fa3f3f852",
+    "url": "/static/js/runtime-main.d6839b17.js"
+  }
+]);
